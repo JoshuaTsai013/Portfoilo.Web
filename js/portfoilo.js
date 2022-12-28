60c51cd1022e3e0d44a5a97596256401169a1679
Joshuatsai <joshuatsai1213@gmail.com>
2022-12-28 15:16:07 +0800

    shop-portRWDV1

--- js/portfoilo.js
+++ js/portfoilo.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
-    // $('.grid').masonry({
-    //     itemSelector: '.grid-item',
-    //     columnWidth: '.grid-sizer',
-    //     percentPosition: true
-
-    // });
-
     $('.row').imagesLoaded().progress( function() {
-        $('.row').masonry('layout');
-      });
+   
+      $('.row').masonry('layout');
+      
+    });
 });
